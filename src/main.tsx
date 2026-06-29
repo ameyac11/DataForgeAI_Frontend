bcf4f8b27913cadcb39ec591a25c7a345285d8e2
ameyac11 <ameyachopade402@gmail.com>
2026-06-29 18:41:51 +0530

    added tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
+import Clarity from "@microsoft/clarity";
 import App from "./App.tsx";
 import "./index.css";
+
+// Initialize Microsoft Clarity
+Clarity.init("xemklukqoy");
 
 createRoot(document.getElementById("root")!).render(<App />);
